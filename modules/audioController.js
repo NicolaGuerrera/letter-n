4079cb387aa2467b6c8c06e4ac97f8feff5cd51f
NicolaGuerrera <nicolaguerrera23@libero.it>
2025-04-01 10:41:20 +0200

    Update audioController.js

--- modules/audioController.js
+++ modules/audioController.js
@@ -1,8 +1,9 @@
 export class AudioController {
-  constructor() {
+  constructor(baseSensitivity = 1) {
     // @ts-ignore
     this.mic = null;
-    this.sensitivity = 1;
+    this.sensitivity = baseSensitivity;
+    this.defaultSensitivity = baseSensitivity;
   }
 
   init() {
@@ -29,6 +30,6 @@ export class AudioController {
   }
 
   resetSensitivity() {
-    this.sensitivity = 1;
+    this.sensitivity = this.defaultSensitivity;
   }
 }
